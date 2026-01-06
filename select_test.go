84e04f9fbafcf65c15a0d456346d93516ef0d80c
Paweł Kierski <pkierski@gmail.com>
2026-01-06 22:48:11 +0100

    docs: Restore explicit predicate function in example

--- select_test.go
+++ select_test.go
@@ -9,7 +9,9 @@ import (
 
 func ExampleSelect() {
 	slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	evenSlice := Select(slice, isEven)
+	evenSlice := Select(slice, func(i int) bool {
+		return i%2 == 0
+	})
 	fmt.Println(evenSlice)
 	// Output: [2 4 6 8 10]
 }
